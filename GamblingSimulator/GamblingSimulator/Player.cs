cf4578833f7d11c71f1959a6b5c1ab57f90614be
yogesh1232 <yogeshkusakiya77@gmail.com>
2022-02-12 18:25:12 +0530

    Player win or loss the game

--- GamblingSimulator/GamblingSimulator/Player.cs
+++ GamblingSimulator/GamblingSimulator/Player.cs
@@ -11,5 +11,17 @@ namespace GamblingSimulator
         // constants
         const int STAKE_PER_DAY = 100;
         const int BET_PER_GAME = 1;
+        const int WIN = 1;
+
+        //Check the player status win or loss
+        public static void GameStatus()
+        {
+            Random random = new Random();
+            int stake = random.Next(1, 3);
+            if (stake == WIN)
+                Console.WriteLine("Playe win the game");
+            else
+                Console.WriteLine("Playe loss the game");
+        }
     }
 }
--- GamblingSimulator/GamblingSimulator/Program.cs
+++ GamblingSimulator/GamblingSimulator/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using GamblingSimulator;
+
 Console.WriteLine("Welcome to Gambler Simulator");
+
+Player.GameStatus();
