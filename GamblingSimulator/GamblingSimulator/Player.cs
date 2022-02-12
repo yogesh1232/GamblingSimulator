f49efbf99f769ee91515d7ab0ec0de19b9aeed1d
yogesh1232 <yogeshkusakiya77@gmail.com>
2022-02-12 18:19:59 +0530

    Game started with stake

---
+++ GamblingSimulator/GamblingSimulator/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblingSimulator
+{
+    public class Player
+    {
+        // constants
+        const int STAKE_PER_DAY = 100;
+        const int BET_PER_GAME = 1;
+    }
+}
